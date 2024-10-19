e78c7add8647016ab200da6176e6d36246b07b21
Desmond <butzebydesi@outlook.com>
2024-10-19 11:23:50 -0700

    Changed feedforward back to 0.5, set proportional to 2.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EmergencyTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EmergencyTeleOP.java
@@ -13,10 +13,10 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 @TeleOp
 public class EmergencyTeleOP extends LinearOpMode {
 
-    public static final double NEW_P = 2.5;
+    public static final double NEW_P = 2.0;
     public static final double NEW_I = 0.1;
     public static final double NEW_D = 0.2;
-    public static final double NEW_F = 0.1;
+    public static final double NEW_F = 0.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
