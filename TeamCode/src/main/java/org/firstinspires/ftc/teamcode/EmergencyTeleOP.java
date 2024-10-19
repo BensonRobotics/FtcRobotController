e88fc6a5bff141c3c527fafb461ade55673d2915
Desmond <butzebydesi@outlook.com>
2024-10-19 14:08:03 -0700

    Set proportional to 1.2, integral to 0.1, derivative to 0, and feedforward to 11.7

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EmergencyTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EmergencyTeleOP.java
@@ -13,10 +13,10 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 @TeleOp
 public class EmergencyTeleOP extends LinearOpMode {
 
-    public static final double NEW_P = 3.0;
-    public static final double NEW_I = 0.3;
-    public static final double NEW_D = 0.3;
-    public static final double NEW_F = 0.5;
+    public static final double NEW_P = 1.2;
+    public static final double NEW_I = 0.1;
+    public static final double NEW_D = 0.0;
+    public static final double NEW_F = 11.7;
 
     @Override
     public void runOpMode() throws InterruptedException {
