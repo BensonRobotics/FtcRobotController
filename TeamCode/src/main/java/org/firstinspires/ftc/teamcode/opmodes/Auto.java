abba17b58390941a4adf06411043dddadcb14321
Desmond <butzebydesi@outlook.com>
2024-10-31 10:57:30 -0700

    Fixed Auto.java, increased distance to travel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
@@ -7,12 +7,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name="Auto1",group="187auto ")
 public class Auto extends LinearOpMode {
-    // PIDF stands for Proportional, Integral, Derivative, Feedforward
-    // PIDF coefficients for drive system's setPosition
-    public static final double NEW_P_DRIVE = 0.5;
-    public static final double NEW_I_DRIVE = 0.1;
-    public static final double NEW_D_DRIVE = 0.1;
-    public static final double NEW_F_DRIVE = 10.0;
 
     public void runOpMode(){
         DcMotorEx frontLeftMotor;
@@ -34,40 +28,34 @@ public class Auto extends LinearOpMode {
 
         // Calculations to convert linear distance in mm to encoder steps
         double stepsPerMMDrive = 537.7/104.0*Math.PI;
-        // Set stepsTraveled to distance in mm converted to encoder steps
+        // Set targetSteps to distance in MM, converted to encoder steps
         // (int) casts float value to int for rounding down
-        int stepsTraveled = (int) (stepsPerMMDrive*200);
+        int targetSteps = (int) (stepsPerMMDrive*1000);
 
         // Zero all motor encoders
         frontLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         frontRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         backLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        frontRightMotor.setTargetPosition(0);
-        frontLeftMotor.setTargetPosition(0);
-        backRightMotor.setTargetPosition(0);
-        backLeftMotor.setTargetPosition(0);
-
-        // Set drive motors' Position Proportional Coefficients
-        frontRightMotor.setPositionPIDFCoefficients(NEW_P_DRIVE);
-        frontLeftMotor.setPositionPIDFCoefficients(NEW_P_DRIVE);
-        backRightMotor.setPositionPIDFCoefficients(NEW_P_DRIVE);
-        backLeftMotor.setPositionPIDFCoefficients(NEW_P_DRIVE);
 
         waitForStart();
 
-        // Encoder on left motors may be reversed by setDirection, may not.
-        // If so, add - sign to left motors' stepsTraveled
-        frontLeftMotor.setTargetPosition(stepsTraveled);
-        backLeftMotor.setTargetPosition(stepsTraveled);
-        frontRightMotor.setTargetPosition(stepsTraveled);
-        backRightMotor.setTargetPosition(stepsTraveled);
+        // Set all motor target positions
+        frontLeftMotor.setTargetPosition(targetSteps);
+        backLeftMotor.setTargetPosition(targetSteps);
+        frontRightMotor.setTargetPosition(targetSteps);
+        backRightMotor.setTargetPosition(targetSteps);
+
+        // Set all motor power levels
+        frontRightMotor.setPower(1);
+        frontLeftMotor.setPower(1);
+        backRightMotor.setPower(1);
+        backLeftMotor.setPower(1);
 
         // Tell all motors to run to target position
         frontRightMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         frontLeftMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         backRightMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         backLeftMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-
     }
 }
