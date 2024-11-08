7a854f487eceaaab89e4f1013a4a0525f077d41f
Desmond <butzebydesi@outlook.com>
2024-11-07 18:00:58 -0800

    Working autonomous test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
@@ -12,23 +12,24 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @Autonomous
 public class Auto extends LinearOpMode {
-    double NEW_POS_P_DRIVE = 1.0;
-    double driveStepsPerMM = 537.7 / (104.0 * Math.PI);
+    double NEW_POS_P_DRIVE = 1.5;
+    double driveStepsPerMM = 537.7 / (103.0 * Math.PI);
     DcMotorEx frontLeftMotor;
     DcMotorEx frontRightMotor;
     DcMotorEx backLeftMotor;
     DcMotorEx backRightMotor;
-    IMU imu = hardwareMap.get(IMU.class, "imu");
+    IMU imu;
     double botHeading = 0;
     boolean useFieldCentric = true;
 
     public void runOpMode() throws InterruptedException {
-        // Declare our motors
+        // Declare our devices
         // Make sure your ID's match your configuration
         frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
         frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
         backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeftMotor");
         backRightMotor = hardwareMap.get(DcMotorEx.class, "backRightMotor");
+        imu = hardwareMap.get(IMU.class, "imu");
 
         frontLeftMotor.setPositionPIDFCoefficients(NEW_POS_P_DRIVE);
         frontRightMotor.setPositionPIDFCoefficients(NEW_POS_P_DRIVE);
@@ -41,17 +42,16 @@ public class Auto extends LinearOpMode {
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Set all motors to brake automatically
-        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         // Zero all motor encoders
         frontLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         frontRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         backLeftMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         backRightMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeftMotor.setTargetPositionTolerance(20);
+        frontRightMotor.setTargetPositionTolerance(20);
+        backLeftMotor.setTargetPositionTolerance(20);
+        backRightMotor.setTargetPositionTolerance(20);
 
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
@@ -59,37 +59,48 @@ public class Auto extends LinearOpMode {
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
         // Without this, the REV Hub's orientation is assumed to be logo up USB forward
         imu.initialize(parameters);
+        imu.resetYaw();
 
         waitForStart();
 
-        driveWithTrigonometry(0,100,0,0.25);
-        wait(500);
-        driveWithTrigonometry(100,0,0,0.25);
-        wait(500);
-        driveWithTrigonometry(-100,-100,0,0.25);
-        wait(500);
+        driveWithTrigonometry(0,500,0,0.25);
+        sleep(500);
+        driveWithTrigonometry(1000,0,0,0.25);
+        sleep(500);
+        driveWithTrigonometry(-1000,-500,0,0.25);
+        sleep(500);
     }
     private void driveWithTrigonometry(int relativeX, int relativeY, double rotation, double power) {
         // Calculate angle and hypotenuse
         double driveAngle = Math.atan2(relativeX, relativeY);
         double driveDistance = Math.hypot(relativeX, relativeY);
 
+        driveDistance *= 1.5;
+
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
         // Run all the motors to target positions
         // Do the loop once, then keep looping until all motors are done
         // I love do-while loops
         do {
             if (useFieldCentric) { // Get the robot rotation value, if using field centric
-                botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+                botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
             }
             // Calculate motor positions
-            double frontLeftBackRightMotors = driveDistance * Math.sin(driveAngle + botHeading + 0.25 * Math.PI);
-            double frontRightBackLeftMotors = driveDistance * -Math.sin(driveAngle + botHeading - 0.25 * Math.PI);
+            double frontLeftBackRightMotors = driveDistance * Math.sin(driveAngle + botHeading + (0.25 * Math.PI));
+            double frontRightBackLeftMotors = driveDistance * -Math.sin(driveAngle + botHeading - (0.25 * Math.PI));
+
+            frontRightBackLeftMotors *= driveStepsPerMM;
+            frontLeftBackRightMotors *= driveStepsPerMM;
 
             // Set all motor target positions
-            frontLeftMotor.setTargetPosition((int) (driveStepsPerMM * frontLeftBackRightMotors + rotation));
-            backLeftMotor.setTargetPosition((int) (driveStepsPerMM * frontRightBackLeftMotors + rotation));
-            frontRightMotor.setTargetPosition((int) (driveStepsPerMM * frontRightBackLeftMotors - rotation));
-            backRightMotor.setTargetPosition((int) (driveStepsPerMM * frontLeftBackRightMotors - rotation));
+            frontLeftMotor.setTargetPosition((int) (frontLeftBackRightMotors + rotation));
+            backLeftMotor.setTargetPosition((int) (frontRightBackLeftMotors + rotation));
+            frontRightMotor.setTargetPosition((int) (frontRightBackLeftMotors - rotation));
+            backRightMotor.setTargetPosition((int) (frontLeftBackRightMotors - rotation));
 
             // Tell all motors to run to target position
             frontRightMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
@@ -102,6 +113,12 @@ public class Auto extends LinearOpMode {
             frontLeftMotor.setPower(power);
             backRightMotor.setPower(power);
             backLeftMotor.setPower(power);
+
         } while (frontRightMotor.isBusy() || frontLeftMotor.isBusy() || backRightMotor.isBusy() || backLeftMotor.isBusy());
+
+        frontRightMotor.setPower(0);
+        frontLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
     }
 }
