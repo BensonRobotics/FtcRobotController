e9fcffa254aba701e0d892001383aee6b1cb12cc
Desmond <82969005+burninghamster58@users.noreply.github.com>
2025-02-24 11:41:21 -0800

    Disabled old auto and ultrasonic test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaAutoSpark2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JavaAutoSpark2025.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -13,6 +14,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
+@Disabled
 @Autonomous
 public class
 JavaAutoSpark2025 extends LinearOpMode {
