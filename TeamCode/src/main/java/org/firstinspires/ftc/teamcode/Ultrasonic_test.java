6fa327da56ab436775f577ba093e820c8739dc57
Desmond <82969005+burninghamster58@users.noreply.github.com>
2025-02-24 11:41:21 -0800

    Disabled old auto and ultrasonic test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ultrasonic_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ultrasonic_test.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 // Imports and package
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.UltrasonicSensor;
@@ -8,6 +9,7 @@ import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 // Class naming and declaration
+@Disabled
 @TeleOp(name="Ultrasonic Test and Calibrate", group="Test")
 public class Ultrasonic_test extends LinearOpMode {
 
