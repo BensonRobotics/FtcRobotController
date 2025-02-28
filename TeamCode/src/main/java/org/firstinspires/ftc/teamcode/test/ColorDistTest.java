d26a9aa4e1c7e88fd539fc0260d2d2590df660cf
Desmond <82969005+burninghamster58@users.noreply.github.com>
2025-02-28 11:06:22 -0800

    Made a rev color distance v1 sensor test auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/ColorDistTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous
+public class ColorDistTest extends LinearOpMode {
+    ColorRangeSensor clawColorDist;
+    public void runOpMode() {
+        while (opModeIsActive()) {
+            telemetry.addData("Red: ", clawColorDist.red());
+            telemetry.addData("Green: ", clawColorDist.green());
+            telemetry.addData("Blue: ", clawColorDist.blue());
+            telemetry.addData("Alpha: ", clawColorDist.alpha());
+            telemetry.addData("ARGB: ", clawColorDist.argb());
+            telemetry.addData("Distance (mm): ", clawColorDist.getDistance(DistanceUnit.MM));
+            telemetry.addData("Normalized Colors: ", clawColorDist.getNormalizedColors());
+            telemetry.addData("Light Detected: ", clawColorDist.getLightDetected());
+            telemetry.addData("Raw Light Detected: ", clawColorDist.getRawLightDetected());
+            telemetry.update();
+        }
+    }
+}
