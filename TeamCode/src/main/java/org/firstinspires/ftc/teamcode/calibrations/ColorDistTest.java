ba0d5d0b17cc88e9e55ac34cdd27aa23a1ecc12a
Desmond <82969005+burninghamster58@users.noreply.github.com>
2025-03-01 11:33:43 -0800

    Added LED control on "A" button

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calibrations/ColorDistTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/calibrations/ColorDistTest.java
@@ -1,16 +1,33 @@
 package org.firstinspires.ftc.teamcode.calibrations;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Autonomous
+@TeleOp
 public class ColorDistTest extends LinearOpMode {
     ColorRangeSensor clawColorDist;
     public void runOpMode() {
-        while (opModeIsActive()) {
+        boolean aDebounce = true; // Debounce for A button press
+        boolean sensorLEDEnabled = false; // Indicator of LED state, can't just get state from sensor
+        clawColorDist.enableLed(sensorLEDEnabled); // Shut off LED at start to comply with FTC rules
+
+        while (opModeIsActive()) { // Loop
+            if (!aDebounce) { // If not debouncing
+                if (gamepad1.a) { // A is pressed
+                    sensorLEDEnabled = !sensorLEDEnabled; // Toggle LED state variable
+                    aDebounce = true; // Debounce
+                }
+            } else { // If debouncing
+                if (!gamepad1.a) { // Reset debounce if not pressed
+                    aDebounce = false; // Reset debounce
+                }
+            }
+            clawColorDist.enableLed(sensorLEDEnabled); // Set LED state from variable
+
+            // Lots of telemetry
             telemetry.addData("Red: ", clawColorDist.red());
             telemetry.addData("Green: ", clawColorDist.green());
             telemetry.addData("Blue: ", clawColorDist.blue());
