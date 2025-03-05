b353183e5a19beb33aee20dcc2e43de01e32b210
Desmond <82969005+burninghamster58@users.noreply.github.com>
2025-03-05 13:38:46 -0800

    HURRAY

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -8,13 +8,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
-        OTOSConstants.useCorrectedOTOSClass = false;
-        OTOSConstants.hardwareMapName = "sensor_otos";
+        OTOSConstants.useCorrectedOTOSClass = true;
+        OTOSConstants.hardwareMapName = "otosCorrected";
         OTOSConstants.linearUnit = DistanceUnit.INCH;
         OTOSConstants.angleUnit = AngleUnit.RADIANS;
-        OTOSConstants.offset = new SparkFunOTOS.Pose2D(0, 0, Math.PI / 2);
-        OTOSConstants.linearScalar = 1.0;
-        OTOSConstants.angularScalar = 1.0;
+        OTOSConstants.offset = new SparkFunOTOS.Pose2D(-6.6875, -3.625, Math.PI / 2);
+        OTOSConstants.linearScalar = 0.99732;
+        OTOSConstants.angularScalar = 0.9814;
     }
 }
 
