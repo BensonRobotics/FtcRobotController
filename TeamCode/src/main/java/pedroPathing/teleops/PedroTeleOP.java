f3f616824ca7e419f359437b18a41ae06c435daa
Desmond <82969005+burninghamster58@users.noreply.github.com>
2025-03-06 14:26:35 -0800

    Removed unused import

--- TeamCode/src/main/java/pedroPathing/teleops/PedroTeleOP.java
+++ TeamCode/src/main/java/pedroPathing/teleops/PedroTeleOP.java
@@ -3,7 +3,6 @@ package pedroPathing.teleops;
 import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
-import com.pedropathing.pathgen.Vector;
 import com.pedropathing.util.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
