510ab7aaa477406eee28ae071626bf11421b11c6
Desmond <82969005+burninghamster58@users.noreply.github.com>
2025-04-09 09:06:19 -0700

    A blank version to copy paste

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptNullOp.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Concept: NullOp", group = "Concept")
+@Disabled
+public class ConceptNullOp extends OpMode {
+
+  @Override
+  public void init() {
+  }
+
+  @Override
+  public void init_loop() {
+  }
+
+  @Override
+  public void start() {
+  }
+
+  @Override
+  public void loop() {
+  }
+
+  @Override
+  public void stop() {
+
+  }
+}
